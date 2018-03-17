bc31e2c870d3b10f30b0f5001aea345395a496b5
Anton Brekhov <tony.brekhov@ya.ru>
2018-03-17 23:45:06 +0300

    Parsing using classes.

--- lect/BasisLib.cpp
+++ lect/BasisLib.cpp
@@ -18,8 +18,6 @@ int BasisLib::loadContent(char* filename, vector<string>& cont){
 
 // TODO same as prev comment
 
-
-
 int BasisLib::findDATA(){
 	vector<string> tmpCont;
 	for ( int i=0; i< (int) content.size();i++){
@@ -47,19 +45,33 @@ int BasisLib::load(char* filename){
 	}
 	int nPos = findDATA();
 	nPos++;
-	cout << nPos <<endl;
+	
+	int bfnum = 0;
 	vector<string> tmpCont;
 	for (int i=nPos; i<(int) content.size(); i++){
 		parseString(content[i], tmpCont);
-		if(tmpCont.size()==1){
+		if(tmpCont.size()==1&&tmpCont[0]!="$END"){	
 			Element element;
-			element.nameElement = tmpCont[i];
+			element.nameElement = tmpCont[0];
+			cout << tmpCont[0]<< endl;
 			superArray.push_back(element);
+		}
+		if(tmpCont.size()==2){
+			SingleBF singlebf;
+			if(tmpCont[0]=="S") singlebf.L=0;
+			else if(tmpCont[0]=="P") singlebf.L=1;
+			else if(tmpCont[0]=="D") singlebf.L=2;
+			
+			superArray.back().bf.push_back(singlebf);
+		}
+		if(tmpCont.size()==3||tmpCont.size()==4){
+			cout << tmpCont[1] << "  " << tmpCont[2] << endl;
+			superArray.back().bf.back().c_Alpha.push_back(make_pair(stod(tmpCont[1]), stod(tmpCont[2])));	
 		}
 	}
 
 	for( int i=0; i< (int) superArray.size(); i++){
-		cout<<superArray[i].nameElement<<endl;
+		//cout<<superArray[i].nameElement<<endl;
 	}
 		
 }
